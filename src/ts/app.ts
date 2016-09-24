14c7e68c0f7677cb19ced7fda61e52ed91fc9149
yuki.h <yuki.h@192.168.11.3>
2016-09-24 23:30:50 +0900

    filter impl

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -1,44 +1,46 @@
 /// <reference path="../typings/tsd.d.ts" />
 
 class DDO implements ng.IDirective {
-  scope = {};
-  restrict = 'E';
-  controller = todoListController;
-    controllerAs = 'ctrl';
-  templateUrl = 'template/todo.html';
+	scope = {};
+	restrict = 'E';
+	controller = todoListController;
+	controllerAs = 'ctrl';
+	templateUrl = 'template/todo.html';
 }
 
 class todoListController {
-    public text:string;
+	public text:string;
 
-    public todos = [
-          {
-              text:'learn angular',
-              time: this.getDate(),
-              done:true
-          },
-          {
-              text:'build an angular app',
-              time: this.getDate(),
-              done:false
-          }
-    ];
+	public todos = [
+		{
+			text:'learn angular',
+			time: this.getDate(),
+			done:true
+		},
+		{
+			text:'build an angular app',
+			time: this.getDate(),
+			done:false
+		}
+	];
 
-  constructor() {}
+	constructor() {}
 
-  //あとでtodoServiceに分ける｡
-  public add(): void {
-      this.todos.push({text:this.text , time: this.getDate(), done:false});
-  }
+	//あとでtodoServiceに分ける｡
+	public add(): void {
+		this.todos.push({text:this.text , time: this.getDate(), done:false});
+	}
 
-  public getDate(): string{
-       var date: Date= new Date();
-      return date.getHours()+"時"+date.getMinutes()+"分";
-  }
+	public getDate(): string{
+		var date: Date= new Date();
+		return date.getHours()+":"+date.getMinutes()+":"+date.getSeconds();
+	}
+
+	public delete(): void{}
 }
 
 (function(){
-  angular
-    .module('app', [])
-    .directive('todoList', () => new DDO())
+	angular
+	.module('app', [])
+	.directive('todoList', () => new DDO())
 })();
