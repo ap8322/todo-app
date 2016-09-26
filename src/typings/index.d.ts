abaf455194e93dedfcff52515728be5f787b6212
yuki.h <yuki.h@192.168.11.3>
2016-09-26 18:01:52 +0900

    tsd migrate typings

---
+++ src/typings/index.d.ts
@@ -0,0 +1,3 @@
+/// <reference path="globals/angular-ui-router/index.d.ts" />
+/// <reference path="globals/angularjs/legacy/angular/index.d.ts" />
+/// <reference path="globals/jquery/index.d.ts" />
